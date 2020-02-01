33166628a6947d9ce2ac2c9ae5af017371ea077c
siyua <siyuanliu3@gmail.com>
2020-02-01 10:43:21 -0500

    changed a view

--- app/src/main/java/com/example/ellehacks/HomeFragment.java
+++ app/src/main/java/com/example/ellehacks/HomeFragment.java
@@ -26,7 +26,9 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_fragment, container, false);
+//        return inflater.inflate(R.layout.home_fragment, container, false);
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        return view;
     }
 
 }
