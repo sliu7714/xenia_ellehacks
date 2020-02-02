135d4f4794de009da8c00fd2399d88c7d0bf8220
kate <siyuanliu3@gmail.com>
2020-02-02 01:24:12 -0500

    added post view fragment

--- app/src/main/java/com/example/ellehacks/ProfileFragment.java
+++ app/src/main/java/com/example/ellehacks/ProfileFragment.java
@@ -1,11 +1,14 @@
 package com.example.ellehacks;
 
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 /**
@@ -23,7 +26,23 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //changing fragment to post details once post is clicked
+        ImageView post = view.findViewById(R.id.pic_00);
+        post.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                HomeActivity.fragmentManager.beginTransaction()
+                        .replace(R.id.FragmentContainer, new PostViewFragment(), null)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+
+        });
+
+        return view;
 
 
 
